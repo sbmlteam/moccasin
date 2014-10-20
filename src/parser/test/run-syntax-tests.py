692ab0dd369bb596a2078293ffade84fa5f77876
Michael Hucka <mhucka@caltech.edu>
2014-10-19 23:05:21 -0400

    Adjusted for new method changes.

--- src/parser/test/run-syntax-tests.py
+++ src/parser/test/run-syntax-tests.py
@@ -4,7 +4,7 @@ import glob, sys
 from pyparsing import ParseException, ParseResults
 sys.path.append('..')
 sys.path.append('../../utilities')
-from matlab_parser import parse_string
+from matlab_parser import parse_matlab_string
 
 for f in glob.glob("syntax-test-cases/valid*.m"):
     print '===== ' + f + ' ' + '='*30
@@ -13,7 +13,7 @@ for f in glob.glob("syntax-test-cases/valid*.m"):
     print contents.rstrip()
     print '----- output ' + '-'*30
     try:
-        parse_string(contents)
+        parse_matlab_string(contents)
     except ParseException as err:
         print("error: {0}".format(err))
     print ''
