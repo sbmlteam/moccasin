fbcccbdc61e9c09894e159548c8e4bca25863456
Michael Hucka <mhucka@caltech.edu>
2014-12-06 20:24:30 -0500

    Huge enhancement.
    1. Added properties and getters and setters.
    2. Create ScopeDict class to hold values that are dictionaries.

--- src/parser/matlab/scope.py
+++ src/parser/matlab/scope.py
@@ -17,57 +17,126 @@
 # file named "COPYING.txt" included with this software distribution and also
 # available online at https://github.com/sbmlteam/moccasin/.
 
+import collections
+from pyparsing import ParseResults
+
+
+# This next class def is courtesy of http://stackoverflow.com/a/7760938/743730
+#
+class ScopeDict(collections.MutableMapping, dict):
+    '''Class used to implement Scope properties that are dictionaries.'''
+
+    def __setitem__(self, key, val):
+        self._dict[key] = val
+
+    def __getitem__(self,key):
+        return dict.__getitem__(self,key)
+
+    def __setitem__(self, key, value):
+        dict.__setitem__(self,key,value)
+
+    def __delitem__(self, key):
+        dict.__delitem__(self,key)
+
+    def __iter__(self):
+        return dict.__iter__(self)
+
+    def __len__(self):
+        return dict.__len__(self)
+
+    def __contains__(self, x):
+        return dict.__contains__(self,x)
+
+
 class Scope:
+
     def __init__(self, name='', parent=None, pr=None, args=[], returns=[]):
-        self.variables     = dict()
-        self.functions     = dict()
-        self.calls         = dict()
-        self.name          = name
-        self.parent        = parent
-        self.args          = args     # If this is a function, its args.
-        self.returns       = returns  # If this is a function, return values.
-        self.comments      = []       # Comments ahead of this function.
+        self.name          = name       # Name of this scope.
+        if isinstance(args, ParseResults):
+            self.args      = args.asList()
+        else:
+            self.args      = args       # If this is a function, its arg list.
+        if isinstance(returns, ParseResults):
+            self.returns   = returns.asList()
+        else:
+            self.returns   = returns    # If this is a function, return values.
+        self.comments      = []         # Comments ahead of this function.
+        self.parent        = parent     # Parent scope containing this one.
         self.parse_results = pr
+        self._functions    = ScopeDict()
+        self._variables    = ScopeDict()
+        self._calls        = ScopeDict()
 
 
     def __repr__(self):
+        parent_name = ''
         if self.parent:
-            p = self.parent.name
-        else:
-            p = ''
+            parent_name = self.parent.name
         s = '<scope "{0}": {1} func defs, {2} vars, {3} calls, parent = "{4}">'
-        return s.format(self.name, len(self.functions), len(self.variables),
-                        len(self.calls), p)
+        return s.format(self.name, len(self._functions), len(self._variables),
+                        len(self._calls), parent_name)
 
 
     def copy_scope(self, source):
         if not isinstance(source, Scope):
             raise TypeError('Expected a Scope object')
-        self.variables     = source.variables
-        self.functions     = source.functions
-        self.calls         = source.calls
         self.name          = source.name
-        self.parent        = source.parent
         self.args          = source.args
         self.returns       = source.returns
         self.comments      = source.comments
+        self.parent        = source.parent
         self.parse_results = source.parse_results
+        self._functions    = source._functions
+        self._variables    = source._variables
+        self._calls        = source.calls
 
 
-    def add_function_definition(self, name, args, output, pr):
-        if args:
-            args = args.asList()
-        if output:
-            output = output.asList()
-        self.functions[name] = {'args': args, 'output': output, 'parse' : pr}
+    @property
+    def functions(self):
+        return self._functions
 
 
-    def add_variable_assignment(self, name, value):
-        self.variables[name] = value
+    @functions.setter
+    def functions(self, key, value):
+        if not isinstance(value, Scope):
+            raise ValueError("Functions values must be Scope objects")
+        else:
+            self._functions[name] = value
 
 
-    def add_function_call(self, name, args):
-        self.calls[name] = args
+    @functions.getter
+    def functions(self):
+        return self._functions
+
+
+    @property
+    def variables(self):
+        return self._variables
+
+
+    @variables.setter
+    def variables(self, key, value):
+        self._variables[key] = value
+
+
+    @variables.getter
+    def variables(self):
+        return self._variables
+
+
+    @property
+    def calls(self):
+        return self._calls
+
+
+    @calls.setter
+    def calls(self, key, value):
+        self._calls[key] = value
+
+
+    @calls.getter
+    def calls(self):
+        return self._calls
 
 
 # Quick testing interface.
