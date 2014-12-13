db5873e9ecad1ba178e27be12b209223bd5cff3a
Michael Hucka <mhucka@caltech.edu>
2014-12-12 20:25:12 -0700

    Added this in the wrong way; going to redo as a git submodule.
    Former-commit-id: b6b03e296e180c0ea13b5e6505b43ba60666c64b
    Former-commit-id: 68922df9a9004905581f52d3e9be2f99773549d8

--- other/matlab-parsers/Mat2py/source/lib/min.py
+++
@@ -1,5 +0,0 @@
-def min(self,curNode):
-    self._CodeGen__emitCode("np.min(")
-    self._CodeGen__genExp(curNode.child[0])
-    self._CodeGen__visitSibling(curNode.child[0],self._CodeGen__genExp,", ")
-    self._CodeGen__emitCode(")")
