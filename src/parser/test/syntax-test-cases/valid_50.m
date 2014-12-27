0007b43f3f614c4225538a5a7240933f324bc1df
Michael Hucka <mhucka@caltech.edu>
2014-12-26 19:23:32 -0500

    Changed to be a more canonical example. Added spaces between operators, changed the variable names to be more aligned with what is going on, and added a comment summarizing what Matlab's ode45 function does.

--- src/parser/test/syntax-test-cases/valid_50.m
+++ src/parser/test/syntax-test-cases/valid_50.m
@@ -1,20 +1,26 @@
 function valid_50
-% parameter values
-% time is in minutes
-tspan=[0 300]; 
-xzero=[0;0];
-% parameter values  (Time is in minutes)
-a=0.01*60;
-b=0.0058*60;
-c=0.006*60;
-d=0.000192*60;
-[t,y]=ode45(@g,tspan,xzero);
-figure
-plot(t,y(:,2))
 
-function dy=g(t,x)
-% ODE for  mRNA and protein production and decay (Thattai-Van Oudenaarden)
-dy=[a-b*x(1); c*x(1)-d*x(2)];
+tspan = [0 300]; 
+xinit = [0; 0];
+
+a = 0.01 * 60;
+b = 0.0058 * 60;
+c = 0.006 * 60;
+d = 0.000192 * 60;
+
+% Matlab's ode45 is designed to handle the following general problem:
+% given a set of differential equations, dX/dt = f(t, X), 
+% with initial values X(t_0) = xinit, it solves for values of the
+% vector of variables X at different times t.  The function defining
+% the differential equations is given as "f".
+
+[t, x] = ode45(@f, tspan, xinit);
+
+figure
+plot(t, y(:, 2))
+
+function dx = f(t, x)
+  dx = [a - b * x(1); c * x(1) - d * x(2)];
 end
 
 end
