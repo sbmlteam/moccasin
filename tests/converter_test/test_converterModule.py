fbe5b1cc84c8891e137f78f361adb87bc9ee8aa0
haroldGomez <rolo.1987@gmail.com>
2015-02-12 13:37:36 +0100

    Added automatic parametrization of test function for converter tests
    Former-commit-id: 14133322c9d41444d07d8d3077cf09cbe6fdee95
    Former-commit-id: 1f7c32968cd9fa53b69a26d9be4095193da08e66

--- tests/converter_test/test_converterModule.py
+++ tests/converter_test/test_converterModule.py
@@ -2,48 +2,59 @@
 
 import pytest
 import sys
+import glob
 from pyparsing import ParseResults
 sys.path.append('../../moccasin/converter/')
 sys.path.append('../../moccasin/')
 from matlab_parser import *
 from converter import *
-    
+
+#Generates (multiple) parametrized calls to a test function
+def pytest_generate_tests(metafunc):
+    # called once per each test function
+    funcarglist = metafunc.cls.params[metafunc.function.__name__]
+    argnames = list(funcarglist[0])
+    metafunc.parametrize(argnames, [[funcargs[name] for name in argnames]
+            for funcargs in funcarglist])
+
+#Parses and converts file to SBML and prints result(output is captured)                       
 def build_model(path):
     file = open(path,'r')
     contents = file.read()
     parser = MatlabGrammar()
-    results = parser.parse_string(contents, fail_soft=True)
-    sbml = create_raterule_model(results, True)
-    file.close()
-    print(sbml)
-
+    try:
+        results = parser.parse_string(contents, fail_soft=True)
+        sbml = create_raterule_model(results, True)
+        file.close()
+        print(sbml)
+    except Exception as e:
+        print e
+   
+#reads file containing expected sbml model and returns as string
 def read_sbml (path):
     file = open(path,'r')
     contents = file.read()
     file.close()
     return contents
-            
-def test_case51(capsys):
-    build_model("converter-test-cases/valid_51.m")
-    out,err=capsys.readouterr()
-    assert out.strip()== read_sbml("converter-test-cases/valid_51.xml").strip()
 
-def test_case52(capsys):
-    build_model("converter-test-cases/valid_52.m")
-    out,err=capsys.readouterr()
-    assert out.strip()== read_sbml("converter-test-cases/valid_52.xml").strip()
+# Constructs the params dictionary for test function parametrization
+def obtain_params():
+    matlab_models=glob.glob("converter-test-cases/*.m")
+    sbml_models=glob.glob("converter-test-cases/*.xml")
+    pairs=list()
+    for i in range(len(matlab_models)):
+        pairs.append((dict(model= matlab_models[i],sbml=sbml_models[i])))
+        params={'test_converterCases':pairs}
+    return params
 
-def test_case53(capsys):
-    build_model("converter-test-cases/valid_53.m")
-    out,err=capsys.readouterr()
-    assert out.strip()== read_sbml("converter-test-cases/valid_53.xml").strip()
+class TestClass:
+    # a map specifying multiple argument sets for a test method
+    params = obtain_params()
+    
+    def test_converterCases(self,capsys, model, sbml):
+        build_model(model)
+        out,err=capsys.readouterr()
+        assert out.strip()== read_sbml(sbml).strip()
 
-def test_case54(capsys):
-    build_model("converter-test-cases/valid_54.m")
-    out,err=capsys.readouterr()
-    assert out.strip()== read_sbml("converter-test-cases/valid_54.xml").strip()
 
-#def test_case55(capsys):
-#    build_model("converter-test-cases/valid_55.m")
-#    out,err=capsys.readouterr()
-#    assert out.strip()== read_sbml("converter-test-cases/valid_55.xml").strip()
+
