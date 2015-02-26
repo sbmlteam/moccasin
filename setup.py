d368e0b73fc0fd0c3e971efffcb86717a6cc507a
haroldGomez <rolo.1987@gmail.com>
2015-02-26 10:37:33 -0500

    Updated license name and added req file to sdist
    Former-commit-id: 1467f9ec8f83c0eb0376ea54fd5e96fcab85c335
    Former-commit-id: 17a2153880e751fb04632f7a20c74ea724b91f0b

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'requirements.txt')) as f:
     reqs = f.read().rstrip().encode("utf-8").splitlines()
 
 class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+    user_options = [('pytest-args=', 'vv', "Arguments to pass to py.test")]
 
     def initialize_options(self):
         TestCommand.initialize_options(self)
@@ -42,8 +42,8 @@ setup(
     cmdclass={'test': PyTest},
     author_email='email@sbml.com',
     description='User-assisted converter that can take MATLAB or Octave ODE-based models in biology and translate them into SBML format',
-    packages=find_packages(exclude='test'),
-##  package_data={'moccasin': ['docs/*.txt','COPYING.txt']},
+    packages=find_packages(exclude='tests'),
+#   package_data={'moccasin': ['docs/*.txt','LICENSE.txt', 'requirements.txt']},
     include_package_data=True,
     platforms='any',
     test_suite='tests',
