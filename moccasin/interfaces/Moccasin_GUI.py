feb7cb99830fa0053dbdf902f9c7d6724eabe147
haroldGomez <rolo.1987@gmail.com>
2015-03-30 16:18:27 -0400

    Added page setup option
    Former-commit-id: a98bf24a465238e73cbb75b2ddc73b9523afca1a
    Former-commit-id: 5569793a23032070500fd66a31547182764463b6

--- moccasin/interfaces/Moccasin_GUI.py
+++ moccasin/interfaces/Moccasin_GUI.py
@@ -130,6 +130,13 @@ def resetOnOpen( self, event ):
         self.convertFile.Enable(1)
         self.convertedTextCtrl.SetValue("")
 
+#Initializes values for printing  
+def initializePrintingDefaults(self):
+        self.pdata = wx.PrintData()
+        self.pdata.SetPaperId(wx.PAPER_LETTER)
+        self.pdata.SetOrientation(wx.PORTRAIT)
+        self.margins = (wx.Point(15,15), wx.Point(15,15))
+
 # -----------------------------------------------------------------------------
 # Global configuration constants
 # -----------------------------------------------------------------------------
@@ -140,6 +147,109 @@ _LICENSE_URL = "https://www.gnu.org/licenses/lgpl.html"
 _VERSION = "Version:  "+ getPackageVersion()
 _IS_OUTPUT_SAVED = False
 _SAVEAS_ODE = False #Used to save the right file format
+_FONTSIZE = 10
+
+# -----------------------------------------------------------------------------
+# Printout class definition
+# -----------------------------------------------------------------------------
+
+#A printout class that is able to print simple text documents.
+#Does not handle page numbers or titles, and it assumes that no
+#lines are longer than what will fit within the page width
+
+class TextDocPrintout(wx.Printout):
+      
+    def __init__(self, text, title, margins):
+        wx.Printout.__init__(self, title)
+        self.lines = text.split('\n')
+        self.margins = margins
+
+
+    def HasPage(self, page):
+        return page <= self.numPages
+
+
+    def GetPageInfo(self):
+        return (1, self.numPages, 1, self.numPages)
+
+
+    def CalculateScale(self, dc):
+        # Scale the DC such that the printout is roughly the same as
+        # the screen scaling.
+        ppiPrinterX, ppiPrinterY = self.GetPPIPrinter()
+        ppiScreenX, ppiScreenY = self.GetPPIScreen()
+        logScale = float(ppiPrinterX)/float(ppiScreenX)
+
+        # Now adjust if the real page size is reduced (such as when
+        # drawing on a scaled wx.MemoryDC in the Print Preview.)  If
+        # page width == DC width then nothing changes, otherwise we
+        # scale down for the DC.
+        pw, ph = self.GetPageSizePixels()
+        dw, dh = dc.GetSize()
+        scale = logScale * float(dw)/float(pw)
+
+        # Set the DC's scale.
+        dc.SetUserScale(scale, scale)
+
+        # Find the logical units per millimeter (for calculating the
+        # margins)
+        self.logUnitsMM = float(ppiPrinterX)/(logScale*25.4)
+
+
+    def CalculateLayout(self, dc):
+        # Determine the position of the margins and the
+        # page/line height
+        topLeft, bottomRight = self.margins
+        dw, dh = dc.GetSize()
+        self.x1 = topLeft.x * self.logUnitsMM
+        self.y1 = topLeft.y * self.logUnitsMM
+        self.x2 = dc.DeviceToLogicalXRel(dw) - bottomRight.x * self.logUnitsMM 
+        self.y2 = dc.DeviceToLogicalYRel(dh) - bottomRight.y * self.logUnitsMM 
+
+        # use a 1mm buffer around the inside of the box, and a few
+        # pixels between each line
+        self.pageHeight = self.y2 - self.y1 - 2*self.logUnitsMM
+        font = wx.Font(_FONTSIZE, wx.TELETYPE, wx.NORMAL, wx.NORMAL)
+        dc.SetFont(font)
+        self.lineHeight = dc.GetCharHeight() 
+        self.linesPerPage = int(self.pageHeight/self.lineHeight)
+
+
+    def OnPreparePrinting(self):
+        # calculate the number of pages
+        dc = self.GetDC()
+        self.CalculateScale(dc)
+        self.CalculateLayout(dc)
+        self.numPages = len(self.lines) / self.linesPerPage
+        if len(self.lines) % self.linesPerPage != 0:
+            self.numPages += 1
+
+
+    def OnPrintPage(self, page):
+        dc = self.GetDC()
+        self.CalculateScale(dc)
+        self.CalculateLayout(dc)
+
+        # draw a page outline at the margin points
+        dc.SetPen(wx.Pen("black", 0))
+        dc.SetBrush(wx.TRANSPARENT_BRUSH)
+        r = wx.RectPP((self.x1, self.y1),
+                      (self.x2, self.y2))
+        dc.DrawRectangleRect(r)
+        dc.SetClippingRect(r)
+
+        # Draw the text lines for this page
+        line = (page-1) * self.linesPerPage
+        x = self.x1 + self.logUnitsMM
+        y = self.y1 + self.logUnitsMM
+        while line < (page * self.linesPerPage):
+            dc.DrawText(self.lines[line], x, y)
+            y += self.lineHeight
+            line += 1
+            if line >= len(self.lines):
+                break
+        return True
+
 
 # -----------------------------------------------------------------------------
 # Graphical User Interface (GUI) definition
@@ -177,6 +287,12 @@ class MainFrame ( wx.Frame ):
                 self.saveFile = wx.MenuItem( self.fileMenu, wx.ID_SAVE, "Save"+ "\t" + "Ctrl+S", wx.EmptyString, wx.ITEM_NORMAL )
                 self.fileMenu.AppendItem( self.saveFile )
                 self.fileMenu.AppendSeparator()
+
+                self.pageSetup = wx.MenuItem( self.fileMenu, wx.ID_PAGE_SETUP, "Page Setup"+ "\t" + "F5", wx.EmptyString, wx.ITEM_NORMAL )
+                self.fileMenu.AppendItem( self.pageSetup )
+                self.fileMenu.AppendSeparator()
+                initializePrintingDefaults(self)# initialize the print data and set some default values
+ 
                 self.exit = wx.MenuItem( self.fileMenu, wx.ID_EXIT, "Exit"+ "\t" + "Alt+F4", wx.EmptyString, wx.ITEM_NORMAL )
                 self.fileMenu.AppendItem( self.exit )
 
@@ -339,6 +455,7 @@ class MainFrame ( wx.Frame ):
                 self.Bind( wx.EVT_MENU, self.onAbout, id = self.about.GetId() )
                 self.Bind( wx.EVT_FILEPICKER_CHANGED, self.onFilePicker, id = self.filePicker.GetId() )
                 self.Bind( wx.EVT_BUTTON, self.onConvert, id = self.convertButton.GetId() )
+                self.Bind(wx.EVT_MENU, self.OnPageSetup, id= self.pageSetup.GetId())
                 self.Bind(wx.EVT_MENU_RANGE, self.onFileHistory, id=wx.ID_FILE1, id2=wx.ID_FILE9)
                 self.Bind( wx.EVT_CLOSE, self.onClose ) 
 
@@ -496,6 +613,22 @@ class MainFrame ( wx.Frame ):
         def onClose( self, event ):
                 checkSaveOutput( self,event )
                 self.Destroy()
+
+        def OnPageSetup(self, evt):
+                data = wx.PageSetupDialogData()
+                data.SetPrintData(self.pdata)
+                data.SetDefaultMinMargins(True)
+                data.SetMarginTopLeft(self.margins[0])
+                data.SetMarginBottomRight(self.margins[1])
+                dlg = wx.PageSetupDialog(self, data)
+
+                if dlg.ShowModal() == wx.ID_OK:
+                        data = dlg.GetPageSetupData()
+                        self.pdata = wx.PrintData(data.GetPrintData()) # force a copy
+                        self.pdata.SetPaperId(data.GetPaperId())
+                        self.margins = (data.GetMarginTopLeft(),
+                                        data.GetMarginBottomRight())
+                dlg.Destroy()
 
 # -----------------------------------------------------------------------------
 # Driver
