eaecd294648e5cee610a00ec2221d149185e841f
Michael Hucka <mhucka@caltech.edu>
2015-08-07 10:55:33 -0700

    Forgot to update the version number in the last release.

--- moccasin/version.py
+++ moccasin/version.py
@@ -20,4 +20,4 @@
 # available online at https://github.com/sbmlteam/moccasin/.
 # ------------------------------------------------------------------------- -->
 
-__version__= '1.0.0'
+__version__= '1.0.2'
