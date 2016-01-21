f886cec3799ef31074ddc2a6fd111dc735095f6d
Michael Hucka <mhucka@caltech.edu>
2016-01-20 19:01:11 -0800

    Changed MOCCASIN acronym.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 #
 # <!---------------------------------------------------------------------------
 # This software is part of MOCCASIN, the Model ODE Converter for Creating
-# Awesome SBML INteroperability. Visit https://github.com/sbmlteam/moccasin/.
+# Automated SBML INteroperability. Visit https://github.com/sbmlteam/moccasin/.
 #
 # Copyright (C) 2014-2015 jointly by the following organizations:
 #  1. California Institute of Technology, Pasadena, CA, USA
@@ -75,7 +75,7 @@ setup(
     install_requires=reqs,
     cmdclass={'test': PyTest},
     author_email='email@sbml.com',
-    description='MOCCASIN: the Model ODE Converter for Creating Awesome SBML INteroperability, a user-assisted converter that can take MATLAB or Octave ODE-based models in biology and translate them into the SBML format.',
+    description='MOCCASIN: the Model ODE Converter for Creating Automated SBML INteroperability, a user-assisted converter that can take MATLAB or Octave ODE-based models in biology and translate them into the SBML format.',
     packages=find_packages(exclude='tests'),
     package_data={'moccasin': ['docs/*.md','LICENSE.txt', 'requirements.txt']},
     include_package_data=True,
