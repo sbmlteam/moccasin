543def91c50d33043a4161ee00b55f052358dd08
Michael Hucka <mhucka@caltech.edu>
2016-05-23 16:51:28 -0700

    Updated copyright year.

--- moccasin/version.py
+++ moccasin/version.py
@@ -8,7 +8,7 @@
 # This software is part of MOCCASIN, the Model ODE Converter for Creating
 # Automated SBML INteroperability. Visit https://github.com/sbmlteam/moccasin/.
 #
-# Copyright (C) 2014-2015 jointly by the following organizations:
+# Copyright (C) 2014-2016 jointly by the following organizations:
 #     1. California Institute of Technology, Pasadena, CA, USA
 #     2. Icahn School of Medicine at Mount Sinai, New York, NY, USA
 #     3. Boston University, Boston, MA, USA
