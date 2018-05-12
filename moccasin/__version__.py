c1cf826cdb02d7ef5b11ae8c0f9a187ba02fac89
Michael Hucka <mhucka@caltech.edu>
2018-05-11 19:55:38 -0700

    Add a docstring

--- moccasin/__version__.py
+++ moccasin/__version__.py
@@ -1,24 +1,25 @@
-#!/usr/bin/env python
-#
-# @file    __version__.py
-# @brief   MOCCASIN version info
-# @author  Michael Hucka
-#
-# <!---------------------------------------------------------------------------
-# This software is part of MOCCASIN, the Model ODE Converter for Creating
-# Automated SBML INteroperability. Visit https://github.com/sbmlteam/moccasin/.
-#
-# Copyright (C) 2014-2017 jointly by the following organizations:
-#     1. California Institute of Technology, Pasadena, CA, USA
-#     2. Icahn School of Medicine at Mount Sinai, New York, NY, USA
-#     3. Boston University, Boston, MA, USA
-#
-# This is free software; you can redistribute it and/or modify it under the
-# terms of the GNU Lesser General Public License as published by the Free
-# Software Foundation.  A copy of the license agreement is provided in the
-# file named "COPYING.txt" included with this software distribution and also
-# available online at https://github.com/sbmlteam/moccasin/.
-# ------------------------------------------------------------------------- -->
+'''
+__version__.py: MOCCASIN version info
+
+This module contains version, author and other info about MOCCASIN.
+
+Copyright
+---------
+
+This software is part of MOCCASIN, the Model ODE Converter for Creating
+Automated SBML INteroperability. Visit https://github.com/sbmlteam/moccasin/.
+
+Copyright (C) 2014-2017 jointly by the following organizations:
+    1. California Institute of Technology, Pasadena, CA, USA
+    2. Icahn School of Medicine at Mount Sinai, New York, NY, USA
+    3. Boston University, Boston, MA, USA
+
+This is free software; you can redistribute it and/or modify it under the
+terms of the GNU Lesser General Public License as published by the Free
+Software Foundation.  A copy of the license agreement is provided in the
+file named "COPYING.txt" included with this software distribution and also
+available online at https://github.com/sbmlteam/moccasin/.
+'''
 
 __version__     = '1.2.0'
 __title__       = 'MOCCASIN'
